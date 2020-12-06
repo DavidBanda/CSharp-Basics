9395f1bed3536d1229e566ad6f1a39713a621efa
David Banda <davidbandag96@gmail.com>
2020-12-05 18:36:24 -0700

    add project basics to the repo

---
+++ Basics/Basics/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Basics
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            /*Create variables and append them to a string*/
+            string characterName = "David";
+            int charaterAge = 24;
+            charaterAge++;
+            char grade = 'a';
+
+            
+            Console.WriteLine($"Hol{grade} {characterName}, de {charaterAge} años");
+        }
+    }
+}
