946b79854b55848081a9a9cf40c453977db8e476
David Banda <davidbandag96@gmail.com>
2020-12-07 10:17:38 -0700

    update the project

--- Basics/Basics/Program.cs
+++ Basics/Basics/Program.cs
@@ -8,12 +8,83 @@ namespace Basics
         {
             /*Create variables and append them to a string*/
             string characterName = "David";
-            int charaterAge = 24;
-            charaterAge++;
+            int characterAge = 24;
+            characterAge++;
             char grade = 'a';
+            int[] numbers = { 1, 2, 3 };
+            string[] friends = new string[4];
+            friends[0] = "David";
+          
+            Console.WriteLine($"Hol{grade} {characterName}, de {characterAge} años");
+            Console.WriteLine(friends);
+            Console.WriteLine(SayHi("David"));
 
-            
-            Console.WriteLine($"Hol{grade} {characterName}, de {charaterAge} años");
+            Console.WriteLine(IsMaleAndTall(true, true));
+
+            if (characterName == "David")
+            {
+                Console.WriteLine("Es true");
+            }
+
+            Console.WriteLine(DayName(2));
+        }
+
+        public static string SayHi(string user)
+        {
+            return $"Hello, {user}";
+        }
+
+        public static string IsMaleAndTall(bool male, bool tall)
+        {
+            string response = "";
+
+            if (male && tall)
+            {
+                response = "Es hombre y es alto";
+            } else if (male && !tall)
+            {
+                response = "Es hombre pero no es alto";
+            } else
+            {
+                response = "Ni es hombre ni es alto";
+            }
+
+            return response;
+        }
+
+        public static string DayName(int day)
+        {
+            string dayName = "";
+
+            switch (day)
+            {
+                case 0:
+                    dayName = "Lunes";
+                    break;
+                case 1:
+                    dayName = "Martes";
+                    break;
+                case 2:
+                    dayName = "Miercoles";
+                    break;
+                case 3:
+                    dayName = "Jueves";
+                    break;
+                case 4:
+                    dayName = "Viernes";
+                    break;
+                case 5:
+                    dayName = "Sabado";
+                    break;
+                case 6:
+                    dayName = "Domingo";
+                    break;
+                default:
+                    dayName = "Ingresa un dia valido";
+                    break;
+            }
+
+            return dayName;
         }
     }
 }
